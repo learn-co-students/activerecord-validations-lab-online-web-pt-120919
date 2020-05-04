52f5ff63db065791d4f2ca9064595e404c8104ac
megfly <mflynn333@gmail.com>
2020-05-04 16:32:50 -0700

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ["Fiction", "Non-Fiction"]}
+    validate :clickbait
+    
+    def clickbait 
+        #if title includes, add error
+        if !title.nil? && !title.include?("Won't Believe" || "Secret" || "Top [number]" || "Guess")
+            errors.add(:title, "Not clickbait-y enough!")
+        end 
+    end 
+
+    
 end
